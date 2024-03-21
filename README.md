8846726e9b499459fc995815eb30203ec40b93da
Mircea Ilie Ploscaru <kid@ml1.net>
2024-03-22 00:01:28 +0100

    k

---
+++ README.md
@@ -0,0 +1,15 @@
+## Work in Progress
+
+Currently overhauling my website.
+
+TODOs:
+
+- [x] simple file serving in zig's zap
+- [ ] templating for articles / pages (dynamic vs static generation?)
+- [ ] clean up CSS
+- [ ] overhaul "about me" page
+- [ ] write up experience of using zig & zap
+
+## Tools needed:
+
+- zig
---
+++ src/main.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const zap = @import("zap");
+const print = std.debug.print;
+
+fn readFileToString(allocator: std.mem.Allocator, file_path: []const u8) !?[]u8 {
+    const file = std.fs.cwd().openFile(file_path, .{}) catch |err| {
+        print("Failed to open file '{s}': {}", .{ file_path, err });
+        return null;
+    };
+    defer file.close();
+
+    const file_contents = file.readToEndAlloc(allocator, std.math.maxInt(usize)) catch |err| {
+        print("Failed to read file '{s}': {}", .{ file_path, err });
+        return null;
+    };
+
+    return file_contents;
+}
+
+fn onRequest(r: zap.Request) void {
+    r.setStatus(.not_found);
+
+    const file_path = "static_old/404.html";
+    const err_404_page = readFileToString(std.heap.page_allocator, file_path) catch |err| {
+        std.log.err("Error: {}", .{err});
+        return;
+    } orelse {
+        std.log.err("File '{s}' is empty or could not be read", .{file_path});
+        return;
+    };
+    defer std.heap.page_allocator.free(err_404_page);
+
+    r.sendBody(err_404_page) catch return;
+}
+
+pub fn main() !void {
+    var listener = zap.HttpListener.init(.{
+        .port = 3000,
+        .on_request = onRequest,
+        .public_folder = "static_old",
+        .log = true,
+    });
+    try listener.listen();
+
+    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
+
+    zap.start(.{
+        .threads = 2,
+        .workers = 2,
+    });
+}
