c60306b5883d39a9f01d3419d133aed3149771c9
Mircea Ilie Ploscaru <kid@ml1.net>
2024-03-29 11:45:56 +0100

    draft caching mechanism

--- src/main.zig
+++ src/main.zig
@@ -2,8 +2,31 @@ const std = @import("std");
 const zap = @import("zap");
 const print = std.debug.print;
 
-// TODO: replace std.mem.Allocator w/ an arena alloc
-fn readFileToString(allocator: std.mem.Allocator, file_path: []const u8) !?[]u8 {
+const MyHashContext = struct {
+    pub fn hash(self: @This(), key: []const u8) u64 {
+        _ = self;
+
+        return std.hash.Wyhash.hash(0, key);
+    }
+
+    pub fn eql(self: @This(), a: []const u8, b: []const u8) bool {
+        _ = self;
+        return std.mem.eql(u8, a, b);
+    }
+};
+
+const FileCache = std.hash_map.HashMap([]const u8, []const u8, MyHashContext, std.hash_map.default_max_load_percentage);
+
+var file_cache = FileCache.init(std.heap.page_allocator);
+
+fn readFileToString(allocator: std.mem.Allocator, file_path: []const u8) !?[]const u8 {
+    if (file_cache.get(file_path)) |file_contents| {
+        print("Serving file '{s}' from cache\n", .{file_path});
+        return file_contents;
+    } else {
+        print("File '{s}' is not cached\n", .{file_path});
+    }
+
     const file = std.fs.cwd().openFile(file_path, .{}) catch |err| {
         print("Failed to open file '{s}': {}", .{ file_path, err });
         return null;
@@ -15,6 +38,13 @@ fn readFileToString(allocator: std.mem.Allocator, file_path: []const u8) !?[]u8 
         return null;
     };
 
+    file_cache.put(file_path, file_contents) catch {
+        print("Failed to cache file '{s}'\n", .{file_path});
+        allocator.free(file_contents);
+        return null;
+    };
+
+    print("File '{s}' cached\n", .{file_path});
     return file_contents;
 }
 
@@ -22,6 +52,7 @@ fn onRequest(r: zap.Request) void {
     r.setStatus(.not_found);
 
     const file_path = "static_old/404.html";
+
     const err_404_page = readFileToString(std.heap.page_allocator, file_path) catch |err| {
         std.log.err("Error: {}", .{err});
         return;
@@ -29,7 +60,6 @@ fn onRequest(r: zap.Request) void {
         std.log.err("File '{s}' is empty or could not be read", .{file_path});
         return;
     };
-    defer std.heap.page_allocator.free(err_404_page);
 
     r.sendBody(err_404_page) catch return;
 }
@@ -41,6 +71,7 @@ pub fn main() !void {
         .public_folder = "static_old",
         .log = true,
     });
+
     try listener.listen();
 
     std.debug.print("\nListening on 0.0.0.0:3000\n", .{});
@@ -49,4 +80,6 @@ pub fn main() !void {
         .threads = 2,
         .workers = 2,
     });
+
+    file_cache.deinit();
 }
