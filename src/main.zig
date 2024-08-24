dda6331d3f095f386425e4126376277c5d1892be
Mircea Ilie Ploscaru <kid@ml1.net>
2024-08-24 19:04:14 +0200

    refactored onRequest

--- src/main.zig
+++ src/main.zig
@@ -132,76 +132,36 @@ fn readFileToString(allocator: std.mem.Allocator, file_path: []const u8) !?[]con
 
 fn onRequest(r: zap.Request) void {
     if (r.path) |the_path| {
-        var file_contents: ?[]const u8 = null;
-        var file_path: []const u8 = ""; // I am going to hell for this
+        var file_path: []const u8 = "";
         var content_type: []const u8 = "text/plain; charset=utf-8"; // default
 
         if (std.mem.eql(u8, the_path, "/") or std.mem.eql(u8, the_path, "")) {
-            file_path = std.fmt.allocPrint(std.heap.page_allocator, "{s}/index.html", .{STATIC_FOLDER}) catch |err| {
-                std.log.err("Error allocating memory for file path: {}", .{err});
-                return;
-            };
-
-            file_contents = readFileToString(std.heap.page_allocator, file_path) catch {
-                // serve 404
-                const file_path_404 = STATIC_FOLDER ++ "/404.html";
-                const err_404_page = readFileToString(std.heap.page_allocator, file_path_404) catch |err_404| {
-                    std.log.err("Error reading 404 page: {}", .{err_404});
-                    return; // Or handle this error in another way
-                    // } orelse unreachable; // We assume 404.html exists and can be read
-                } orelse {
-                    r.setStatus(.not_found);
-                    r.sendBody("404") catch return;
-                };
-
-                r.setStatus(.not_found);
-                r.sendBody(err_404_page) catch return;
-            };
+            file_path = std.fmt.allocPrint(std.heap.page_allocator, "{s}/index.html", .{STATIC_FOLDER}) catch return;
         } else if (std.mem.indexOf(u8, the_path, ".")) |_| {
-            file_path = std.fmt.allocPrint(std.heap.page_allocator, "{s}/{s}", .{ STATIC_FOLDER, the_path }) catch |err| {
-                std.log.err("Error allocating memory for file path: {}", .{err});
-                return;
-            };
-
-            file_contents = readFileToString(std.heap.page_allocator, file_path) catch {
-                // serve 404
-                const file_path_404 = STATIC_FOLDER ++ "/404.html";
-                const err_404_page = readFileToString(std.heap.page_allocator, file_path_404) catch |err_404| {
-                    std.log.err("Error reading 404 page: {}", .{err_404});
-                    return; // Or handle this error in another way
-                    // } orelse unreachable; // We assume 404.html exists and can be read
-                } orelse {
-                    r.setStatus(.not_found);
-                    r.sendBody("404") catch return;
-                };
-
-                r.setStatus(.not_found);
-                r.sendBody(err_404_page) catch return;
-            };
+            file_path = std.fmt.allocPrint(std.heap.page_allocator, "{s}/{s}", .{ STATIC_FOLDER, the_path }) catch return;
         } else {
-            file_path = std.fmt.allocPrint(std.heap.page_allocator, "{s}/{s}.html", .{ STATIC_FOLDER, the_path }) catch |err| {
-                std.log.err("Error allocating memory for file path: {}", .{err});
-                return;
-            };
-
-            file_contents = readFileToString(std.heap.page_allocator, file_path) catch {
-                // serve 404
-                const file_path_404 = STATIC_FOLDER ++ "/404.html";
-                const err_404_page = readFileToString(std.heap.page_allocator, file_path_404) catch |err_404| {
-                    std.log.err("Error reading 404 page: {}", .{err_404});
-                    return; // Or handle this error in another way
-                } orelse unreachable;
-
-                r.setStatus(.not_found);
-                r.sendBody(err_404_page) catch return;
-            };
+            file_path = std.fmt.allocPrint(std.heap.page_allocator, "{s}/{s}.html", .{ STATIC_FOLDER, the_path }) catch return;
         }
 
+        const file_contents = readFileToString(std.heap.page_allocator, file_path) catch {
+            // Handle the error and serve 404
+            const file_path_404 = STATIC_FOLDER ++ "/404.html";
+            const err_404_page = readFileToString(std.heap.page_allocator, file_path_404) catch |err_404| {
+                std.log.err("Error reading 404 page: {}", .{err_404});
+                return;
+            } orelse {
+                r.setStatus(.not_found);
+                r.sendBody("404") catch return;
+            };
+
+            r.setStatus(.not_found);
+            r.sendBody(err_404_page) catch return;
+        };
+
         if (file_contents) |contents| {
-            // content_type = MIME_TYPES.get(the_path) orelse "application/octet-stream";
             var extension = std.fs.path.extension(file_path);
             if (extension.len > 0) {
-                extension = extension[1..]; // Remove the leading dot
+                extension = extension[1..];
             }
             for (mime_types_map) |mime_type| {
                 if (std.mem.eql(u8, mime_type.file_type, extension)) {
@@ -212,15 +172,6 @@ fn onRequest(r: zap.Request) void {
 
             r.setHeader("Content-Type", content_type) catch return;
             r.sendBody(contents) catch return;
-        } else {
-            const file_path_404 = STATIC_FOLDER ++ "/404.html";
-            const err_404_page = readFileToString(std.heap.page_allocator, file_path_404) catch |err_404| {
-                std.log.err("Error reading 404 page: {}", .{err_404});
-                return;
-            } orelse unreachable;
-
-            r.setStatus(.not_found);
-            r.sendBody(err_404_page) catch return;
         }
     }
 }
